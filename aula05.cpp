819ea1fc690305375b629cffac9a5fd92440807d
silvaleal <lealsilva.ctt@outlook.com>
2026-03-10 14:29:56 -0300

    aula05 & aula06

---
+++ aula05.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+// constantes
+#define ROOMNAME "aula05"; // old
+const string APPNAME = "silvadaslangs";
+
+int main() {
+    int vidas;
+    int tiros;
+    int life;
+
+    // Declaração múltiplas de variáveis
+    // int vidas, tiros, life;
+    // int vidas=3, tiros=100, life=100;
+
+    cout << APPNAME;
+    cout << "\n";
+    cout << ROOMNAME;
+
+    return 0;
+}
---
+++ aula06.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+string appName = "myapp"; // Variáveis globais
+
+// Operadores
+// + -> Soma
+// - -> Subtração
+// / -> Divisão
+// * -> Multiplicação
+// % -> Resto da divisão
+
+int main() {
+
+    int n1,n2; // Variáveis locais
+
+    cout << appName;
+
+    return 0;
+}
