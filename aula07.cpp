a2c3522a666f8d56dc5b93a579e22605ae043975
silvaleal <lealsilva.ctt@outlook.com>
2026-03-10 17:13:08 -0300

    aula07 & aula08

---
+++ aula07.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int n1, n2;
+
+    n1=0;
+    n2=10;
+
+    // Incremento / Decremento
+    n1 = n1+1;
+    n1+=1;
+    n1++;
+
+    return 0;
+}
---
+++ aula08.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int num=1;
+
+    cout << num << "\n";
+
+    num = num *-1;
+    // num=-num
+
+    cout << num;
+
+    return 0;
+}
